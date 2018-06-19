4e429dda4758dccadf79ca32e5f46d1f2edd8f4a
768680109 <768680109@qq.com>
2018-06-19 11:17:25 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -5,8 +5,6 @@ import Router from 'vue-router'
 const Index = resolve => require(['@/views/index'],resolve);
 
 const Lunbo = resolve => require(['@/components/lunbo'], resolve);
-const Dialog = resolve => require(['@/components/dialog'], resolve);
-const Choujiang = resolve => require(['@/components/choujiang/choujiang'], resolve);
 /*----------------------------------*/
 Vue.use(Router)
 
